f2a1278397d7da7c4747b95906dcb8dedcbdd505
= <craig@craigshoemaker.net>
2018-05-10 14:22:07 -0400

    Spinefeed initial state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'demo-app',
     templateUrl: require('./app.component.html'),
     styleUrls: [require('./app.component.scss')]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    ngOnInit() {
+
+    }
 }
